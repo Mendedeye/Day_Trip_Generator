e06b8b8b7ee7486f0291a235da1b194db87f20bd
Ethan Doering <ethanwdoering@icloud.com>
2023-02-10 13:33:22 -0700

    Added the lists for destinations, restaurants, transportation, and entertainment

---
+++ day_trip_generator.py
@@ -0,0 +1,4 @@
+destination_list = ["New York City", "Toranto", "Miami", "London", "Paris", "Moscow", "Death Valley", "Detroit", "Las Vagas", "Mexico City"]
+reastaurant_list = ["McDonalds", "Ramsy's Steakhouse", "Crumble Cookies", "Wendies", "Chick-Fil-A", "Sonic", "Cracker Barrel", "Outback"]
+transportation_list = ["Semi-truck", "SUV", "Sports Car", "Boat", "Plane", "Scooter", "Bike", "Train", "Helecopter"]
+entertainment_list = ["Rave", "Classical Ball", "Magician", "Inspirational Speaker", "Football", "MMA Fight", "Music Concert"]
